7c2fbe67753ee45cf6fbe49d0c975f7ac9606542
ZenNg <zenwing@gmail.com>
2024-10-11 23:25:23 +0900

    CORS

--- api.task.io.image/app/main.py
+++ api.task.io.image/app/main.py
@@ -1,5 +1,6 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers import image_controller, local_image_controller
 from app.models.database import init_db
 from app.config import logger  # Import logger
@@ -9,6 +10,22 @@ from dotenv import load_dotenv  # Import to load environment variables
 load_dotenv()
 
 app = FastAPI()
+
+# Define the list of allowed origins
+allowed_origins = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:3000",
+    "http://ec2-43-201-64-153.ap-northeast-2.compute.amazonaws.com:3000",  # Example: Your production frontend domain
+]
+
+# Add CORSMiddleware to allow the defined origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,  # Allows requests from specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Initialize logging
 logger.info("Starting FastAPI application...")
