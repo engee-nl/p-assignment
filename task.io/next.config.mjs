cbc1f435f48573d888c2e041b3be3effff0cf23e
ZenNg <zenwing@gmail.com>
2024-10-12 00:11:04 +0900

    upload fixg

--- task.io/next.config.mjs
+++ task.io/next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['ec2-43-201-64-153.ap-northeast-2.compute.amazonaws.com'], 
+        remotePatterns: [
+        {
+            protocol: 'http',
+            hostname: 'ec2-43-201-64-153.ap-northeast-2.compute.amazonaws.com', 
+            port: '8001',
+            pathname: '/image/compressed/**',
+        },
+        ],
     },
-  };
+};
 
 export default nextConfig;
