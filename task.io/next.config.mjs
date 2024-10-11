7b9d6e6ce53d6e75cab830af128577ab400ab2dc
ZenNg <zenwing@gmail.com>
2024-10-12 00:00:59 +0900

    upload fixg

--- task.io/next.config.mjs
+++ task.io/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['ec2-43-201-64-153.ap-northeast-2.compute.amazonaws.com'], 
+    },
+  };
 
 export default nextConfig;
