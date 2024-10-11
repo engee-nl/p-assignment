d1e1b5bd6021bfe8fa4b10427cb4622c42b264e5
ZenNg <zenwing@gmail.com>
2024-10-11 17:05:09 +0900

    error handler change

--- api.task.io.image/app/services/local_image_service.py
+++ api.task.io.image/app/services/local_image_service.py
@@ -86,7 +86,7 @@ async def process_and_save_image(file: UploadFile):
     except Exception as e:
         logger.error(f"Error calculating MD5 hash: {e}")
         error_detail = "Error calculating MD5 hash : " + e
-        raise HTTPException(status_code=500, detail=error_detail)
+        raise HTTPException(status_code=500, detail=str(error_detail))
 
     # Step 3: Resize and convert the image after saving the original
     try:
